40aa17a00b99ac6b9d094b13aeaf81a9a9c34d64
Lorenzo Chelini <l.chelini@icloud.com>
2023-07-20 13:43:22 +0200

    init

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ void _mlir_ciface_fat_gemm(MemRef<float, 2> *inputA,
 void _mlir_ciface_mha_projection_v(MemRef<float, 3> *inputA, MemRef<float, 4> *output);
 void _mlir_ciface_mha_projection_q(MemRef<float, 3> *inputA, MemRef<float, 4> *output);
 void _mlir_ciface_q_times_k(MemRef<float, 4> *inputA, MemRef<float, 4> *inputB, MemRef<float, 4> *output);
+void _mlir_ciface_softmax_times_v(MemRef<float, 4> *inputA, MemRef<float, 4> *inputB, MemRef<float, 4> *output);
 }
 
 static void BM_fat_gemm(benchmark::State& state) {
@@ -103,12 +104,36 @@ static void BM_mha_q_times_k(benchmark::State& state) {
   for (auto _ : state) {
     _mlir_ciface_q_times_k(&inputA, &inputB, &output);
   }
+}
 
+static void BM_softmax_times_v(benchmark::State& state) {
+  intptr_t sizesInputA[4] = {64,8,32,32};
+  MemRef<float, 4> inputA(sizesInputA);
+  intptr_t sizesInputB[4] = {64,32,8,64};
+  MemRef<float, 4> inputB(sizesInputB);
+  MemRef<float, 4> output(sizesInputB);
+
+  random_device rd;
+  mt19937 gen(rd());
+  uniform_real_distribution<> dis(0.0, 1.0);
+
+  for (int i = inputA.getSize(); i >= 0; i--) {
+    inputA[i] = dis(gen);
+  }
+
+  for (int i = inputB.getSize(); i >= 0; i--) {
+    inputB[i] = dis(gen);
+  }
+
+  for (auto _ : state) {
+    _mlir_ciface_softmax_times_v(&inputA, &inputB, &output);
+  }
 }
 
 BENCHMARK(BM_fat_gemm);
 BENCHMARK(BM_mha_projection_v);
 BENCHMARK(BM_mha_projection_q);
 BENCHMARK(BM_mha_q_times_k);
+BENCHMARK(BM_softmax_times_v);
 
 BENCHMARK_MAIN();
--- run-me.sh
+++ run-me.sh
@@ -15,26 +15,47 @@ llc main.ll
 tpp-opt -default-pipeline mlir/fat-gemm.mlir > fat-gemm.llvm.mlir
 mlir-translate fat-gemm.llvm.mlir -mlir-to-llvmir > fat-gemm.ll
 llc fat-gemm.ll
+# FLOPS = 32 * 32 * 32 * 2
+# ~108 GFLOPs
+# 100% peak (baseline)
 
 #projection_v.
 tpp-opt -tile-consumer-and-fuse-producers -convert-linalg-to-tpp -bufferize \
   -convert-linalg-to-xsmm -default-pipeline mlir/projection_v.mlir > projection_v.llvm.mlir
 mlir-translate projection_v.llvm.mlir -mlir-to-llvmir > projection_v.ll
 llc projection_v.ll
+# FLOPS = 64 * 32 * 8 * 64 * 512 (red) * 2 = 1073741824
+# ~52 GFLOPs
+# 48% peak
 
 #projection q.
 tpp-opt -tile-consumer-and-fuse-producers -convert-linalg-to-tpp -bufferize \
   -convert-linalg-to-xsmm -default-pipeline mlir/projection_q.mlir > projection_q.llvm.mlir
 mlir-translate projection_q.llvm.mlir -mlir-to-llvmir > projection_q.ll
 llc projection_q.ll
+# FLOPS = 64 * 32 * 8 * 64 * 512 (red) * 2 = 1073741824
+# ~53 GFLOPs
+# 49% peak
 
-# q times v
+# q times k.
 tpp-opt -tile-consumer-and-fuse-producers -convert-linalg-to-tpp -bufferize \
   -convert-linalg-to-xsmm -default-pipeline mlir/q_times_k.mlir > q_times_k.llvm.mlir
 mlir-translate q_times_k.llvm.mlir -mlir-to-llvmir > q_times_k.ll
 llc q_times_k.ll
+# FLOPS = 64 * 32 * 8 * 64 * 32 (red) * 2 = 67108864
+# ~59 GFLOPs
+# 54% peak
 
-clang -std=c++11 -O3 main.s fat-gemm.s projection_v.s projection_q.s q_times_k.s \
+# softmax times v
+tpp-opt -tile-consumer-and-fuse-producers -convert-linalg-to-tpp -bufferize \
+  -convert-linalg-to-xsmm -default-pipeline mlir/softmax_times_v.mlir > softmax_times_v.llvm.mlir
+mlir-translate softmax_times_v.llvm.mlir -mlir-to-llvmir > softmax_times_v.ll
+llc softmax_times_v.ll
+# FLOPS = 64 * 32 * 8 * 64 * 32 (red) * 2 = 67108864
+# ~68 GFLOPs
+# 62% peak
+
+clang -std=c++11 -O3 main.s fat-gemm.s projection_v.s projection_q.s q_times_k.s softmax_times_v.s \
   -Lbenchmark/build/src -L../tpp-sandbox/build/lib -no-pie -lstdc++ -lbenchmark -ltpp_c_runner_utils -lm -o main
 
 taskset -c 1 ./main --benchmark_enable_random_interleaving=true --benchmark_repetitions=10 --benchmark_min_time=1s
